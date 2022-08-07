7a4ecf7c8267df99816aba8a1b588f4f37a66d43
Anh Thang Bui <buianhthang89@gmail.com>
2022-08-07 22:15:42 +0700

    fix: client-side rendering, target static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
+  target: "static",
+
   css: ['@/assets/main.less'],
 
   modules: ['@pinia/nuxt', '@nuxtjs/color-mode', '@nuxtjs/google-fonts'],
