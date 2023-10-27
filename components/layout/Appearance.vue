44bc4d1ba719339e5eccb9ec166901c1f08ee14a
Anh Thang Bui <buianhthang89@gmail.com>
2023-10-27 13:11:48 +0700

    fix: click to change appearance theme

--- components/layout/Appearance.vue
+++ components/layout/Appearance.vue
@@ -1,51 +1,35 @@
 <template>
-  <a-dropdown>
-    <a-button type="text">
-      <icon class="light-dark-icon" name="line-md:light-dark" />
-    </a-button>
-    <template #overlay>
-      <a-menu
-        :selected-keys="[$colorMode.preference]"
-        @click="handleAppearanceChange"
-      >
-        <a-menu-item key="system">
-          <icon
-            class="appearance-icon"
-            name="material-symbols:desktop-mac-outline"
-          />
-          System
-        </a-menu-item>
-        <a-menu-item key="light">
-          <icon
-            class="appearance-icon"
-            name="material-symbols:light-mode-outline"
-          />
-          Light
-        </a-menu-item>
-        <a-menu-item key="dark">
-          <icon
-            class="appearance-icon"
-            name="material-symbols:dark-mode-outline"
-          />
-          Dark
-        </a-menu-item>
-      </a-menu>
-    </template>
-  </a-dropdown>
+  <a-button type="text" @click="handleAppearanceChange">
+    <icon class="light-dark-icon" :name="iconName" />
+  </a-button>
 </template>
 
 <script setup>
 const colorMode = useColorMode()
-const handleAppearanceChange = (e) => {
-  colorMode.preference = e.key
+
+const handleAppearanceChange = () => {
+  if (colorMode.preference === 'system') {
+    colorMode.preference = 'light'
+  } else if (colorMode.preference === 'light') {
+    colorMode.preference = 'dark'
+  } else {
+    colorMode.preference = 'system'
+  }
 }
+
+const iconName = computed(() => {
+  switch (colorMode.preference) {
+    case 'dark':
+      return 'material-symbols:dark-mode-outline'
+    case 'light':
+      return 'material-symbols:light-mode-outline'
+    default:
+      return 'material-symbols:desktop-mac-outline'
+  }
+})
 </script>
 
 <style>
-.appearance-icon {
-  font-size: 20px;
-}
-
 .light-dark-icon {
   font-size: 20px;
   color: rgba(255, 255, 255, 0.85);
